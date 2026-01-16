1a60738c75a5e704ee6f37e3328ac8ba4d3c0fda
Anshu010412 <Anshujhacoc@gmail.com>
2026-01-16 16:01:50 +0530

    changes done

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,4 @@
 //create server
-
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
@@ -7,21 +6,22 @@ const cors = require("cors");
 const app = express();
 const connectToDB = require("./db/db");
 connectToDB();
-const userRoutes = require('./routes/user.route');
-const captainRoutes = require('./routes/captain.route')
+const userRoutes = require("./routes/user.route");
+const captainRoutes = require("./routes/captain.route");
 const cookieParser = require("cookie-parser");
+const mapRoutes = require("./routes/maps.route");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use('/users',userRoutes)
-app.use('/captains',captainRoutes)
-
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+app.use("/maps", mapRoutes);
 
 module.exports = app;
